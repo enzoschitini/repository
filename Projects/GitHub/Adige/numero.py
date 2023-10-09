b141c37780cbc2ffcde5c47ad8e607ff572f2282
Enzo <schitinienzo@gmail.com>
2023-10-09 16:49:24 -0300

    first commit

--- Projects/GitHub/Adige/numero.py
+++ Projects/GitHub/Adige/numero.py
@@ -52,3 +52,50 @@ class Util(object):
    def devid(self, num):
       num2 = num / 2
       return num2
+
+def limpstring(string : str, elem : str) -> str:
+   lista = list(string)
+   cancel = 0
+   for i in lista:
+      if i == elem:
+         cancel = 1
+   return cancel
+
+class Usuario(object):
+
+    def __init__(self, nome, idade) -> None:
+        self.nome = nome
+        self.idade = idade
+    
+    def mostrar_proficao(self, proficao):
+        print(f"Nome: {self.nome} e profição: {proficao}")
+    
+    def maior_idade(self):
+        if self.idade >= 18:
+            print("Maior de idade")
+        else:
+            print("Menor de idade")
+
+class ArquivoCSV(object):
+
+  def __init__(self, arquivo: str):
+    self.arquivo = arquivo
+    self.conteudo = self._extrair_conteudo()
+    self.colunas = self._extrair_nome_colunas()
+
+  def _extrair_conteudo(self):
+    conteudo = None
+    with open(file=self.arquivo, mode='r', encoding='utf8') as arquivo:
+      conteudo = arquivo.readlines()
+    return conteudo
+
+  def _extrair_nome_colunas(self):
+    return self.conteudo[0].strip().split(sep=',')
+
+  def extrair_coluna(self, indice_coluna: str):
+    coluna = list()
+    for linha in self.conteudo:
+      conteudo_linha = linha.strip().split(sep=',')
+      coluna.append(conteudo_linha[indice_coluna])
+    coluna.pop(0)
+    return coluna
